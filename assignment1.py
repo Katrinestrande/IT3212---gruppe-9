46eec924581c6dbde0f1ee1a65d2966f4208c408
katrinesr <katrisro@stud.ntnu.no>
2026-09-11 09:10:08 +0200

    Added summary statistics and data types  to assignment1
    Added initial data exploration

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 def data_exploration():
-
     df = pd.read_csv("graduation_dataset.csv")
+
+    # First few rows
+    print("First few rows:")
     print(df.head())
+
+    # Summary statistics
+    print("\nSummary statistics:")
+    print(df.describe()) 
+
+    # Data types
+    print("\nData types:")
+    print(df.dtypes)
 
 data_exploration()
